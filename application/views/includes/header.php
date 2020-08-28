9228f9469fbec848d257ad1c6c0c8454676a74c4
Krishnendu <official.krishnendu@gmail.com>
2020-08-28 13:32:47 +0530

    First commit

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -24,7 +24,6 @@
     <link href="https://fonts.googleapis.com/css2?family=Cookie&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800;900&display=swap"
     rel="stylesheet">
-
     <!-- Css Styles -->
     <link rel="stylesheet" href="<?php echo base_url();  ?>assets/css/bootstrap.min.css" type="text/css">
     <link rel="stylesheet" href="<?php echo base_url();  ?>assets/css/font-awesome.min.css" type="text/css">
